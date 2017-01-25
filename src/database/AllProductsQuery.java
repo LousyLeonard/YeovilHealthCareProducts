81e6e006c8db45e378755a53a9b6e8fafacfb4e0
tommy.hamblin <tommy.hamblin@hotmail.co.uk>
2017-01-25 18:30:08 +0000

    Incorporate the SQL into Java classes #32

---
+++ src/database/AllProductsQuery.java
@@ -0,0 +1,48 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import core.Product;
+
+public class AllProductsQuery {
+
+	private static final String SELECT_STATEMENT = "SELECT p.Product_Name, b.Brand_Name, p.Product_Price, i.Image_Filepath FROM YeovilHealthcare.Product AS p ";
+	private static final String IMAGE_INNER_JOIN = "INNER JOIN Image AS i ON i.Product_ID = p.Product_ID;";
+	private static final String BRAND_INNER_JOIN = "INNER JOIN Brand AS b ON b.Brand_ID = p.Brand_ID ";
+
+	public ArrayList<Product> getAllProducts()
+	{
+		final ArrayList<Product> products = new ArrayList<Product>();
+
+		try
+		{
+			final Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/YeovilHealthcare", "root", "password");
+			final String query = SELECT_STATEMENT + BRAND_INNER_JOIN + IMAGE_INNER_JOIN;
+			final Statement stmt = conn.createStatement();
+			final ResultSet rs = stmt.executeQuery(query);
+
+			while(rs.next())
+			{
+				System.out.println("Product Name: " + rs.getString("Product_Name") + " ... " + "Product Price: " + rs.getString("Product_Price"));
+				final Product product = new Product();
+				product.setProductName(rs.getString("Product_Name"));
+				product.setBrand(rs.getString("Brand_Name"));
+				product.setProductPrice(Double.parseDouble(rs.getString("Product_Price")));
+				product.addImagePath(rs.getString("Image_Filepath"));
+
+				products.add(product);
+			}
+		}
+		catch (final Exception e)
+		{
+			System.err.println(e);
+		}
+
+		return products;
+	}
+
+}
