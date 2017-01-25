0a8417242b4cea10e8ede60b616e435f80d4b4f7
Lawrence Coles <lawrencejcoles@gmail.com>
2017-01-25 14:53:51 +0000

    Display a product from the servlet #34

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -9,6 +9,7 @@ public class HelloWorld extends HttpServlet {
                     HttpServletResponse response)
       throws ServletException, IOException {
     PrintWriter out = response.getWriter();
-    out.println("Hello World");
+    out.println("<div class=\'col-sm-4\'><div class=\'product-image-wrapper\'><div class=\'single-products\'><div class=\'productinfo text-center\'><img src=\'images/home/product1.jpg\' alt=\'\' /><h2>$56</h2><p>FROM SERVLET</p><a href=\'#\' class=\'btn btn-default add-to-cart\'><i class=\'fa fa-shopping-cart\'></i>Add to cart</a></div><div class=\'product-overlay\'><div class=\'overlay-content\'><h2>$56</h2><p>Easy Polo Black Edition</p><a href=\'#\' class=\'btn btn-default add-to-cart\'><i class=\'fa fa-shopping-cart\'></i>Add to cart</a></div></div></div></div></div>");
+//   out.println("<p>test<p>");
   }
 }
