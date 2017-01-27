b5361db45522ed244ab2bfe41de6c6cd721c481f
thejo <thejoshy01@live.co.uk>
2017-01-27 13:53:39 +0000

    Created Brand class

---
+++ src/core/Brand.java
@@ -0,0 +1,23 @@
+package core;
+
+public class Brand {
+
+	private String brand;
+	
+	public Brand() {
+		this.brand = "";
+	}
+	
+	public Brand(String brand) {
+		this.brand = brand;
+	}
+	
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	
+}
