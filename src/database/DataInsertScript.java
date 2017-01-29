b4eda823dc81d814524a4e8e4e5ab8767d2a1749
Lawrence Coles <lawrencejcoles@gmail.com>
2017-01-29 15:04:09 +0000

    Implement SQL INSERT scripts into Java Classes #37
    Some review comments

--- src/database/DataInsertScript.java
+++ src/database/DataInsertScript.java
@@ -18,6 +18,12 @@ public class DataInsertScript {
 
 		try
 		{
+			// Generate new unique Id's
+			IDGenerator generator = IDGenerator.getInstance();
+			// Must only call me once per entry! if a product gets added in between calls I'll change! 
+			// (for ex lines 34 and 36)
+			Integer productID = generator.getNewUniqueID(Table.PRODUCT);
+			
 			Class.forName(DatabaseConstants.JDBC_DRIVER);
 
 			// Creates a database connection with the Yeovilhealthcare database
@@ -25,9 +31,12 @@ public class DataInsertScript {
 			final Connection conn = DriverManager.getConnection(DatabaseConstants.DATABASE_CONNECTION, DatabaseConstants.DATABASE_USERNAME, DatabaseConstants.DATABASE_PASSWORD);
 
 			// Builds the Insert script query
+			/** okay so brand is funny, you'll need to check whether it exists before creating a new one! */
 			final String brandInsert = DatabaseConstants.BRAND_TABLE_INSERTS  + product.getBrand() + DatabaseConstants.CLOSE;
 			final String productInsert = DatabaseConstants.PRODUCT_TABLE_INSERTS  + product.getProductName() + DatabaseConstants.COMMA + product.getProductPrice() + DatabaseConstants.COMMA + (IDGenerator.getInstance().getNewUniqueID(Table.BRAND)) + DatabaseConstants.CLOSE;
+			/** TODO although we've said one image for now that could change, iterate over the array! new entry for each*/
 			final String imageInsert = DatabaseConstants.IMAGE_TABLE_INSERTS  + product.getImagePaths() + DatabaseConstants.COMMA + (IDGenerator.getInstance().getNewUniqueID(Table.PRODUCT)) + DatabaseConstants.CLOSE;
+			/** TODO keywords wont work like this. You need a new keyword entry for each keyword, iterate over the array!*/
 			final String keywordInsert = DatabaseConstants.KEYWORD_TABLE_INSERTS  + product.getKeywords() + DatabaseConstants.CLOSE;
 			final String productKeywordInsert = DatabaseConstants.PRODUCT_KEYWORD_INSERTS  + (IDGenerator.getInstance().getNewUniqueID(Table.PRODUCT)) + DatabaseConstants.COMMA + (IDGenerator.getInstance().getNewUniqueID(Table.KEYWORD)) + DatabaseConstants.CLOSE;
 
